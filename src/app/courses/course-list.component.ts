7b10648e497265ba2648815cda5c77aa0afecfba
evandro <ebolzan@inf.ufsm.br>
2021-09-07 18:07:57 -0300

    injection dependency - lesson

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
   selector: 'app-course-list',
@@ -8,28 +9,13 @@ import { Course } from "./course";
 export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
-  ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 23,
-        duration: 10,
-        rating: 2,
-        releaseDate: 'December, 2, 2019',
-      },
 
-      {
-        id: 2,
-        name: 'http',
-        imageUrl: '/assets/images/http.png',
-        price: 34.6,
-        duration: 1,
-        rating: 5,
-        releaseDate: 'August, 2, 2019',
-      }
-    ]
+  constructor(private courseService: CourseService) {
+
+  }
+
+  ngOnInit(): void {
+    this.courses = this.courseService.retrieveAll();
   }
 
 }
---
+++ src/app/courses/course.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from "@angular/core";
+import { Course } from "./course";
+
+@Injectable(
+  {
+    providedIn: 'root',
+  }
+)
+export class CourseService {
+
+  retrieveAll(): Course[] {
+    return COURSES;
+  }
+
+}
+
+var COURSES: Course[] =
+  [
+    {
+      id: 1,
+      name: 'Forms',
+      imageUrl: '/assets/images/forms.png',
+      price: 23,
+      duration: 10,
+      rating: 2,
+      releaseDate: 'December, 2, 2019',
+    },
+
+    {
+      id: 2,
+      name: 'http',
+      imageUrl: '/assets/images/http.png',
+      price: 34.6,
+      duration: 1,
+      rating: 5,
+      releaseDate: 'August, 2, 2019',
+    },
+    {
+      id: 3,
+      name: 'h2',
+      imageUrl: '/assets/images/forms.png',
+      price: 23,
+      duration: 10,
+      rating: 3,
+      releaseDate: 'December, 2, 2019',
+    },
+
+    {
+      id: 4,
+      name: 'div',
+      imageUrl: '/assets/images/http.png',
+      price: 34.6,
+      duration: 1,
+      rating: 1,
+      releaseDate: 'August, 2, 2019',
+    }
+  ]
